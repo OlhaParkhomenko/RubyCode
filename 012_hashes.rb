0458073e0104e68e5ddda79c07b015bc71697957
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-03-30 00:28:54 +0300

    task12

---
+++ 012_hashes.rb
@@ -0,0 +1,13 @@
+user={:firstname=>"Vasya", :lastname =>"Pupkin", :age=>"22"}
+p user
+
+
+def hashImproved (user)
+  user.each {|key, value| puts "# #{key.capitalize}: #{value}" }
+end
+
+hashImproved (user)
+
+user[:age]="50"
+puts "Hash with new value"
+hashImproved (user)
