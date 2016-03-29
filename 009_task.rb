2ac24c386dab9bb35a9fc43edcec271c8669e75a
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-03-29 13:16:31 +0300

    task9 modifed

--- 009_task.rb
+++ 009_task.rb
@@ -1,10 +1,16 @@
 array =Array.new(10) { |i| rand(100) }
 p array
-minimum = array.min
-maximum = array.max
 sum=0
+minimum = 100
+maximum = 0
 array.each do |i|
   sum+=i
+  if i<minimum 
+    minimum=i
+  end
+  if i>maximum
+    maximum=i
+  end  
 end
 average = sum/10
 puts "minimum=#{minimum}, maximum=#{maximum}, average=#{average}"
