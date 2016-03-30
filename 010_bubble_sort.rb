27966c990829ba5af33e12fe60efdac16b92377b
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-03-30 16:51:17 +0300

    task10

---
+++ 010_bubble_sort.rb
@@ -0,0 +1,16 @@
+def bubble_sort(array)
+	j=1
+	while j<array.length-1
+	0.upto(array.length-2) {|i| 
+		if array[i]>array[i+1] 
+			array[i], array[i+1] = array[i+1], array[i]
+		end	
+	}
+	j+=1
+	end
+end 
+
+array =Array.new(100) { |i| rand(100) }
+p array
+bubble_sort(array)
+p array
