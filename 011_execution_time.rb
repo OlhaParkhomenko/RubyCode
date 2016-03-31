69ccddd2c6e5c2c5e178d303f1aa2a8ce1df5641
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-03-31 20:30:21 +0300

    task11

--- 011_execution_time.rb
+++ 011_execution_time.rb
@@ -2,7 +2,7 @@ require "./011_bubble_sort_ar"
 require "./010_bubble_sort1"
 require 'date'
 
-array=Array.new(20000) { rand(100) }
+array=Array.new(10000) { rand(100) }
 
 method1Start=Time.now
 array.sort
@@ -11,13 +11,13 @@ method1=method1Finish-method1Start
 puts "Sort with standart function = #{method1}"
 
 method2Start=Time.now
-bubble_sort(array)
+bubble_sort(array.dup)
 method2Finish=Time.now
 method2=method2Finish-method2Start
 puts "Bubble sort = #{method2}"
 
 method3Start=Time.now
-bubble_sort_ar(array)
+bubble_sort_ar(array.dup)
 method3Finish=Time.now
 method3=method3Finish-method3Start
 puts "Bubble sort with array = #{method3}"
