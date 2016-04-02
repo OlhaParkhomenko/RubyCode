4552c4d3d5e0fcf23efe1007d57717ede84197ee
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-02 18:09:26 +0300

    task14 with error

---
+++ 014_mixin.rb
@@ -0,0 +1,44 @@
+class Array
+
+ def bubble_sort(array)
+  array2=array.dup
+  loop do
+    swapped = false
+    0.upto(array2.length-2) {|i| 
+      if array2[i]>array2[i+1] 
+       array2[i], array2[i+1] = array2[i+1], array2[i]
+       swapped = true
+     end 
+   }
+   break unless swapped
+ end
+
+
+end  
+
+
+def bubble_sort!
+ loop do
+  swapped = false
+  0.upto(self.length-2) {|i| 
+    if self[i]>self[i+1] 
+     self[i], self[i+1] = self[i+1], self[i]
+     swapped = true
+   end 
+ }
+ break unless swapped
+end 
+self
+end
+
+
+end 
+
+array = Array.new(20) { rand(100) }
+p array
+
+puts bubble_sort(array)
+puts p.array
+
+puts array.bubble_sort!
+puts p.array
