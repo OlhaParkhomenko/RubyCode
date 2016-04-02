562e46a17847e34fa36eb75848994eb40e7b8370
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-03 01:39:02 +0300

    task 14 modified

--- 014_mixin.rb
+++ 014_mixin.rb
@@ -4,32 +4,32 @@ class Array
   array2=self.dup
   loop do
     swapped = false
-    0.upto(array2.length-2) {|i| 
-      if array2[i]>array2[i+1] 
+    0.upto(array2.length-2) {|i|
+      if array2[i]>array2[i+1]
        array2[i], array2[i+1] = array2[i+1], array2[i]
        swapped = true
-      end 
-    }  
+      end
+    }
    break unless swapped
   end
     array2
-  end  
+  end
 
 def bubble_sort!
  loop do
   swapped = false
-  0.upto(self.length-2) {|i| 
-    if self[i]>self[i+1] 
+  0.upto(self.length-2) {|i|
+    if self[i]>self[i+1]
      self[i], self[i+1] = self[i+1], self[i]
      swapped = true
-    end 
+    end
   }
   break unless swapped
- end 
+ end
   self
 end
 
-end 
+end
 
 array = Array.new(20) { rand(100) }
 p array
