9c2a346c5864e4e8e39fdcb2fe75abac0323e8dc
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-04 14:01:02 +0300

    task13

--- 013_hashes2.rb
+++ 013_hashes2.rb
@@ -11,7 +11,10 @@ lastnames =['Bond', 'Ivanov', 'Ivanova', 'Sidorov', 'Sidorova',
               age: rand(10..20)} 
 end
 number = 1  
-array.each {|i| puts "#{number} " +"#{i[:name]} " + "#{i[:lastname]} " + "#{i[:age]} " +"years old"
+array.each {|i| puts "#{number} #{i[:name]} #{i[:lastname]} #{i[:age]} years old"
             number+=1}
 puts "Sorted array"
-pp array.sort_by {|key, value| key[:lastname]}
+sorted = array.sort_by {|key, value| key[:lastname]}
+number = 1  
+sorted.each {|i| puts "#{number}  #{i[:name]} #{i[:lastname]} #{i[:age]} years old"
+            number+=1}
