47d1248d76bcd298226188a0d72345c955ddd7a8
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-04 13:22:28 +0300

    task13 sorted

---
+++ 013_hashes2.rb
@@ -0,0 +1,15 @@
+array =[]
+names =['John', 'Jake', 'Olga', 'Stas', 'Alex', 'Nina', 'Dima',
+      'Paul', 'Sophie', 'Tanya']
+lastnames =['Bond', 'Ivanov', 'Ivanova', 'Sidorov', 'Sidorova',
+      'Shevchenko', 'Boyko', 'Melnik', 'Slon', 'Fesenko']
+
+i=0
+  while i<30  
+    array << {name: names[rand(names.count-1)], lastname: lastnames[rand(lastnames.count-1)], 
+              age: rand(10..20)} 
+    i+=1
+  end
+number = 1  
+array.each {|i| puts "#{number} " +"#{i[:name]} " + "#{i[:lastname]} " + "#{i[:age]} " +"years old"
+            number+=1}
