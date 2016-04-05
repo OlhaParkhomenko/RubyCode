0fad6e62ab27a7a57f740266ba3c30300dc24f56
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-05 20:46:53 +0300

    task19

---
+++ 019_class.rb
@@ -0,0 +1,22 @@
+class Person
+
+  attr_reader :name, :lastname, :age
+
+  def initialize(name, lastname, age)
+    @name=name
+    @lastname=lastname
+    @age=age
+  end
+
+  def to_s
+    "#{@name} #{@lastname} #{@age} years old"
+  end
+    
+end
+
+person=Person.new('Vasya', 'Pupkin', '13')
+
+puts person
+puts person.name
+puts person.lastname
+puts person.age
