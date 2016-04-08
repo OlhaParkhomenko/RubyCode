6730e2f7f1360ed9d2ca1155f810eb14bf1f5dc0
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-08 15:38:32 +0300

    task15

--- 015_task.rb
+++ 015_task.rb
@@ -2,11 +2,11 @@ class FactorialException < ArgumentError
 end
 
 def factorial(n)
-  if n<0 
-    raise FactorialException.new
-  end
-
+   
+  raise FactorialException.new if n<0
+  
   n>1? n*factorial(n-1) :1
+
 end
 
 puts factorial(0)
