e6f846f6e0846f656cd8242d2ffeff12e611e9f6
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-09 22:51:07 +0300

    task20

--- 020_class.rb
+++ 020_class.rb
@@ -1,10 +1,31 @@
 class Person
 
-  attr_reader :name, :lastname, :age
-  attr_writer :name, :lastname, :age
-
   def initialize
   end
+
+  def name
+    @name
+  end
+
+  def lastname
+    @lastname
+  end
+
+  def age
+    @age
+  end
+
+  def name=(name_value)
+    @name=name_value
+  end
+
+  def lastname=(lastname_value)
+    @lastname=lastname_value
+  end
+
+  def age=(age_value)
+    @age=age_value
+  end  
 
   def to_s
     "#{@name} #{@lastname} #{@age} years old"
