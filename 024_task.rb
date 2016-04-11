a1e142097e1b79507f1d52cbf6890082be21148c
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-11 12:10:41 +0300

    task24

--- 024_task.rb
+++ 024_task.rb
@@ -7,10 +7,12 @@ class Point
   end
 end
 
-p1 = Point.new
-p p1
-p2 = Point.new(10, 20)
-p p2
-p1.x=1
-p p1
-p p2.y
+if __FILE__==$0 
+  p1 = Point.new
+  p p1
+  p2 = Point.new(10, 20)
+  p p2
+  p1.x=1
+  p p1
+  p p2.y
+end
