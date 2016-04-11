7684553dc7f3c5ca026e3e49fb456f3fd8e3ba6a
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-11 18:08:39 +0300

    task24

--- 024_task.rb
+++ 024_task.rb
@@ -2,8 +2,8 @@ class Point
   attr_accessor :x, :y
 
   def initialize(x=0, y=0)
-    @x=x
-    @y=y
+    @x = x
+    @y = y
   end
 end
 
@@ -12,7 +12,7 @@ if __FILE__==$0
   p p1
   p2 = Point.new(10, 20)
   p p2
-  p1.x=1
+  p1.x = 1
   p p1
   p p2.y
 end
