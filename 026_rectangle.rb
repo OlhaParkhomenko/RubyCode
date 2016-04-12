9dcd3f4816077e1fa8aa09d0c5374112d5a14ac9
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-12 11:58:52 +0300

    task26

--- 026_rectangle.rb
+++ 026_rectangle.rb
@@ -1,9 +1,9 @@
 require "./025_shape.rb"
 
 class Rectangle < Shape
-  attr_accessor :x, :y, :w, :h
+  attr_accessor :w, :h
 
-  def initialize(x=0, y=0, w=4, h=2)
+  def initialize(x=0, y=0, w=0, h=0)
     super(x, y)
     @w = w
     @h = h      
@@ -14,23 +14,29 @@ class Rectangle < Shape
   end
 
   def center
-    @point = [@position.x + @w/2, @position.y + @h/2]
+    [@position.x + @w/2, @position.y + @h/2]
   end
 
   def bound
     self
   end
 
+  def to_s
+    "Rectangle: x= #{@position.x}, y=#{@position.y}, w= #{@w}, h= #{@h}"
+  end
+
 end
 
-rectangle1 = Rectangle.new
-p rectangle1
-puts "Square = #{rectangle1.area}"
-puts "Center = #{rectangle1.center}"
-puts "Bound result: #{rectangle1.bound} width - #{rectangle1.w}, height - #{rectangle1.h}"
+if __FILE__==$0
+  rectangle1 = Rectangle.new
+  p rectangle1
+  puts "Square = #{rectangle1.area}"
+  puts "Center = #{rectangle1.center}"
+  puts rectangle1.bound
 
-rectangle2 = Rectangle.new(8, 12, 8, 4)
-p rectangle2
-puts "Square = #{rectangle2.area}"
-puts "Center = #{rectangle2.center}"
-puts "Bound result: #{rectangle2.bound} width - #{rectangle2.w}, height - #{rectangle2.h}"
+  rectangle2 = Rectangle.new(8, 12, 8, 4)
+  p rectangle2
+  puts "Square = #{rectangle2.area}"
+  puts "Center = #{rectangle2.center}"
+  puts rectangle2.bound
+end
