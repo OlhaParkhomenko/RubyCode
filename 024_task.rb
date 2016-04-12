dd390aa02956d605181a9dcfc37bb36758d4875b
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-12 11:23:19 +0300

    task24

--- 024_task.rb
+++ 024_task.rb
@@ -5,14 +5,19 @@ class Point
     @x = x
     @y = y
   end
+
+  def to_s
+    "Point: x = #{@x}, y = #{@y}"
+  end
+
 end
 
 if __FILE__==$0 
   p1 = Point.new
-  p p1
+  puts p1
   p2 = Point.new(10, 20)
-  p p2
+  puts p2
   p1.x = 1
-  p p1
-  p p2.y
+  puts p1
+  puts p2.y
 end
