fc55b345c6ea5f55ee7553052f7f08357c0a4ac1
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-18 18:57:18 +0300

    task30_2

--- 030_2_file.rb
+++ 030_2_file.rb
@@ -1,4 +1,11 @@
 fileStrings = IO.read("pyf.svg")
 array = fileStrings.scan(/\sd="([^"]+)"/)[0][0]
-arrayEdit = array.scan(/([A-Z]\s[\d,.\s]+)\s/).flatten.map{|i| i.strip.split(" ")}
-p arrayEdit
+@arrayEdit = array.scan(/([A-Z]\s[\d,.\s]+)\s/).flatten.map{|i| i.strip.split(" ")}
+
+def getArray
+  @arrayEdit
+end
+
+if __FILE__ == $0
+  p getArray
+end
