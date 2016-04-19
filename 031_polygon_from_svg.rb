196bdd2b346e1ac7276594ac39c49d62302cf191
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-19 15:43:38 +0300

    task31

--- 031_polygon_from_svg.rb
+++ 031_polygon_from_svg.rb
@@ -5,30 +5,29 @@ require "./030_2_file.rb"
 class Polygon
 
   def self.polygon_from_svg_path_elements(array)
-    @points=[]
-    array.each{|step|
+
+    Polygon.new(array.map {|step|
       case step[0]
         when 'M'
-          @points << Point.point_from_string(step[1])
+          Point.point_from_string(step[1])
         when 'L'
-          @points << Point.point_from_string(step[1])
+          Point.point_from_string(step[1])
         when 'C'
-          @points << Point.point_from_string(step[3])
+          Point.point_from_string(step[3])
       end
-    }
-    p @points
+      })
   end  
 
 end
 
 class Point
+
   def self.point_from_string(str)
-    @x, @y = str.split(',').map do
-      |c| c.to_f
-    end
-  end
-  Point.new(@x, @y)
+    x, y = str.split(',').map {|c| c.to_f}
+    Point.new(x, y)
+  end  
+  
 end
 
 array = read_path_from_svg("pyf.svg")
-Polygon.polygon_from_svg_path_elements(array)
+p Polygon.polygon_from_svg_path_elements(array)
