e8272a2810a884be06ec155c18b4eac1d88aef4e
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-19 10:04:03 +0300

    task30_2

--- 030_2_file.rb
+++ 030_2_file.rb
@@ -1,11 +1,11 @@
-fileStrings = IO.read("pyf.svg")
-array = fileStrings.scan(/\sd="([^"]+)"/)[0][0]
-@arrayEdit = array.scan(/([A-Z]\s[\d,.\s]+)\s/).flatten.map{|i| i.strip.split(" ")}
+def read_path_from_svg(filename)
 
-def getArray
-  @arrayEdit
+  fileStrings = IO.read(filename)
+  array = fileStrings.scan(/\sd="([^"]+)"/)[0][0]
+  arrayEdit = array.scan(/([A-Z]\s[\d,.\s]+)\s/).flatten.map{|i| i.strip.split(" ")}
+
 end
 
 if __FILE__ == $0
-  p getArray
+  p read_path_from_svg("pyf.svg")
 end
