03cf0e0a62eb1352afbc1c823cd8bd900e67d09d
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-21 22:07:52 +0300

    task32

---
+++ 032_polygon_from_svg_str.rb
@@ -0,0 +1,17 @@
+require "./029_polygon.rb"
+require "./031_polygon_from_svg.rb"
+
+class Polygon
+
+  def self.polygon_from_svg_path_str(string)
+    array = string.scan(/([A-Z]\s[\d,.\s]+)\s/).flatten.map{|i| i.strip.split(" ")} 
+    p string.class
+    p Polygon.polygon_from_svg_path_elements(array)
+  end
+  
+end
+
+string = "M 1863.0115,749.40627 L 1863.0115,748.10024
+C 1852.5619,736.34445 1859.0928,732.42592 1861.7051,720.67013"
+
+p Polygon.polygon_from_svg_path_str(string)
