7787bde83f13d29fcdb7b1398a3cc0e037c9f01d
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-21 22:50:09 +0300

    task30_2

--- 030_2_file.rb
+++ 030_2_file.rb
@@ -2,7 +2,7 @@ def read_path_from_svg(filename)
 
   fileStrings = IO.read(filename)
   array = fileStrings.scan(/\sd="([^"]+)"/)[0][0]
-  arrayEdit = array.scan(/([A-Z]\s[\d,.\s]+)\s/).flatten.map{|i| i.strip.split(" ")}
+  arrayEdit = array.scan(/([A-Z]\s[\d,.\s]+)/).flatten.map{|i| i.strip.split(" ")}
 
 end
 
