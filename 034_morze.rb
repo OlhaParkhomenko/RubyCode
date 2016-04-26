aae7564d5935001d10b01055af35ca4ff7273ced
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-04-26 11:48:26 +0300

    task 34

--- 034_morze.rb
+++ 034_morze.rb
@@ -1,24 +1,25 @@
 module Transformation
   
-  hash_table_morze = Hash.new{A => ".-", B =>"-..." , C => "-.-.", D => "-..", E => ".", F => "..-.", 
-      G => "--.", H => "....", I => "..", J => ".---", K => "-.-", L => ".-..", 
-      M => "--", N => "-.", O => "---", P => ".--.", Q => "--.-", R => ".-.", S => "...",
-      T => "-", U => "..-", V => "...-", W => ".--", X => "-..-", Y => "-.--", Z => "--..",
+  @@table_morze = {"A" => ".-", "B" =>"-..." , "C" => "-.-.", "D" => "-..", "E" => ".", 
+    "F" => "..-.", "G" => "--.", "H" => "....", "I" => "..", "J" => ".---", "K" => "-.-", 
+    "L" => ".-..", "M" => "--", "N" => "-.", "O" => "---", "P" => ".--.", "Q" => "--.-", 
+    "R" => ".-.", "S" => "...", "T" => "-", "U" => "..-", "V" => "...-", "W" => ".--", 
+    "X" => "-..-", "Y" => "-.--", "Z" => "--.."
   }
   
-  hash_table_english = hash_table_morze.invert
+  @@table_english = @@table_morze.invert
   
   def transformation_eng_morze(str)
     
-    str.upcase!.each_char{|ch| ch.sub(hash_table_morze.fetch(ch))}
+    str.upcase.chars.map{|ch| puts @@table_morze[ch]? @@table_morze[ch] : ch }.join
      
   end
 
   def transformation_morze_eng(array)
     raise ("Invalid data") unless array.class == Array 
   
-    array.each{|elem| elem.sub(hash_table_english.fetch(elem))}
-    array.join(',')  
+    p @@table_english
+    array.map{|elem| puts @@table_english[elem]}.join
 
   end
 
@@ -31,5 +32,7 @@ class Morze
   
 end
 
+
 morze = Morze.new
-p morze.transformation_eng_morze("abc")
+morze.transformation_eng_morze("English is language")
+morze.transformation_morze_eng([".-.", "..-", "-...", "-.--"])
