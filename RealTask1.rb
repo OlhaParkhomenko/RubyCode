3e94a32491b657fc479b0366cab60b908d3febd0
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-06-09 20:39:29 +0300

    RealTask method1

--- RealTask1.rb
+++ RealTask1.rb
@@ -1,19 +1,31 @@
 require 'docx'
 
-doc = Docx::Document.open('tables.docx')
+def from_file_to_array(filename)
 
-first_table = doc.tables[0]
-puts first_table.row_count
-puts first_table.column_count
-#puts first_table.rows[0].cells[0].text
-#puts first_table.columns[0].cells[0].text
+  doc = Docx::Document.open(filename)
 
-doc.tables.each do |table|
-  table.rows.each do |row| # Row-based iteration
-    row.cells.each do |cell|
-      puts cell.text
+  first_table = doc.tables[0]
+  
+  i=0
+  array_b=[]
+  while i<first_table.row_count do
+    j=0
+    array_a=[]
+    while j<first_table.column_count do
+      array_a.push(first_table.rows[i].cells[j].text)
+      j+=1
     end
+      array_b.push(array_a)
+      i+=1
   end
+
+  output_file = File.open("./results.txt", 'w')
+  array_b.each {|line|
+    output_file.puts line
+    output_file.puts '\n'
+  }
+
 end
 
-doc.save('yes.docx')
+
+from_file_to_array('tables.docx')
