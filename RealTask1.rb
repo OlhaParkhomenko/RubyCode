549499d68b739e3ec766a84bae2d77cbfcdab0fd
Olha Parkhomenko <olha.parkhomenko@gmail.com>
2016-06-10 13:52:34 +0300

    RealTask method1and2 not finished

--- RealTask1.rb
+++ RealTask1.rb
@@ -1,6 +1,7 @@
 require 'docx'
+require 'erb'
 
-def from_file_to_array(filename)
+def from_table_to_array(filename)
 
   doc = Docx::Document.open(filename)
 
@@ -19,7 +20,7 @@ def from_file_to_array(filename)
       i+=1
   end
 
-  output_file = File.open("./results.txt", 'w')
+  output_file = File.open("./results.txt", 'w+')
   array_b.each {|line|
     output_file.puts line
     output_file.puts '\n'
@@ -27,5 +28,44 @@ def from_file_to_array(filename)
 
 end
 
+def from_array_to_table(array)
+  @array = array
 
-from_file_to_array('tables.docx')
+  @table_row=@array.count
+  table_column=@array[0].count
+
+  template = %{
+    <html>
+      <head>
+        <title>From array to table</title>
+      </head>
+      <body>
+        <table>
+          <% @array[0].each do |f| %>
+            <th><%= f %></th>
+          <% end %> 
+          <% i=1 %>
+          <% while i<@table_row %>
+            <tr>
+              <% @array[i].each do |j| %>
+                <td><%= j %></td>
+              <% end %> 
+            </tr>
+            <% i+=1 %>
+          <% end %>  
+        </table>
+      </body>
+    </html>
+  }
+
+  rhtml = ERB.new(template).run
+
+  #file_table = File.open('output_table.html','w+')
+
+end
+
+
+from_table_to_array('tables.docx')
+from_array_to_table([['value00', 'value01', 'value02'], ['value10', 'value11', 'value12'],
+  ['value20', 'value21','value22'], ['value30','vlue31','value32']])
+
